fc2a3a11314b1503de11fec551921f2e6efbd917
Speedysnail6 <computertutor12@gmail.com>
2014-06-15 20:46:53 -0700

    Added the basic demo
    This includes:
    editable sections
    set the default for a certain section
    set the category of a section (this will be implemented later on)
    edit button on site

---
+++ demos/basic.php
@@ -0,0 +1,24 @@
+<?php include_once('../speedysite.php'); ?>
+<!DOCTYPE html>
+<html>
+<body>
+<?php editbutton(); ?>
+<div id="container" style="width: 100%">
+
+<div id="header" style="background-color:#FFA500;">
+<h1 style="margin-bottom:0;"><?php ss('header'); ?></h1></div>
+
+<div id="menu" style="background-color:#FFD700;height:600px;width: 20%;float:left;">
+<?php ss('menu', 'headersection'); ?>
+</div>
+
+<div id="content" style="background-color:#EEEEEE;height:600px;width:80%;float:left;">
+<?php ss('content', NULL, NULL, 'This is the default text for the content zone'); ?></div>
+
+<div id="footer" style="background-color:#FFA500;clear:both;text-align:center;">
+<?php ss('footer'); ?></div>
+
+</div>
+
+</body>
+</html> 
